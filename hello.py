131fe39421ea51f9073407ad6694f877dfc8c7bd
ishida <ishida@matsumuradenki.co.jp>
2016-10-27 18:33:46 +0900

    hello

--- hello.py
+++ hello.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+message = 'hello world'
+
 @app.route('/')
 def hello():
-    return 'hello world'
+    return message
+
+@app.route('/message')
+def messaging():
+    message = 'text'
+    return hello()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
